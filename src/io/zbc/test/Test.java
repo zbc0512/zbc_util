6cc9950d2ea002ae5a89b759363cec7aa0e3dca3
ZhangBaicheng <zbc0512@163.com>
2016-01-13 10:06:39 +0800

    publish

---
+++ src/io/zbc/test/Test.java
@@ -0,0 +1,10 @@
+package io.zbc.test;
+
+import static io.zbc.util.Print.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Object a = "test";
+        print(a);
+    }
+}
---
+++ src/io/zbc/util/Print.java
@@ -0,0 +1,7 @@
+package io.zbc.util;
+
+public class Print {
+    public static void print(Object obj){
+        System.out.print(obj);
+    }
+}
