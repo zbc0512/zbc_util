4da5a2c780d9ea9480ea515c53fb14d6fb60017e
ZhangBaicheng <zbc0512@163.com>
2016-01-13 17:26:18 +0800

    update Test.java

--- src/io/zbc/test/Test.java
+++ src/io/zbc/test/Test.java
@@ -1,10 +1,13 @@
 package io.zbc.test;
 
 import static io.zbc.util.Print.*;
+import static io.zbc.util.StringUtil.*;
 
 public class Test {
     public static void main(String[] args) {
         Object a = "test";
-        print(a);
+        if (isNotEmpty(a.toString())) {
+            print(a);
+        }
     }
 }
